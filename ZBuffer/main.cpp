624d07a1338c03e91f85a6e6668e473f8612639b
Wenlong Meng <59216317+GYolanda@users.noreply.github.com>
2021-11-12 14:22:12 +0800

    debug macOS

--- ZBuffer/main.cpp
+++ ZBuffer/main.cpp
@@ -3,7 +3,7 @@
 //  ZBuffer
 //
 //  Created by 孟文龙 on 2021/11/11.
-//
+//r
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <stb_image.h>
@@ -12,6 +12,11 @@
 #include <iostream>
 #include <fstream>
 
+
+// settings
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+#define DIM     600
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -25,21 +30,33 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
-// settings
-const unsigned int SCR_WIDTH = 800;
-const unsigned int SCR_HEIGHT = 600;
+
+void kernel( unsigned char* ptr, int width, int height ) {
+    for (int y=0; y<width; y++) {
+        for (int x=0; x<height; x++) {
+            int offset = x + y*height;
+            
+            ptr[offset*4 + 0] = 255;
+            ptr[offset*4 + 1] = 255;
+            ptr[offset*4 + 2] = 0;
+            ptr[offset*4 + 3] = 255;
+        }
+    }
+}
 
 
 int main(int argc, const char * argv[]) {
     
     //初始化glfw
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint一旦加上，glDrawPixels便无法使用
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    //glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
     //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_COMPAT_PROFILE);
+    // wocore，这里还必须使用core
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);
 #ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
     
     // glfw window creation
@@ -63,11 +80,19 @@ int main(int argc, const char * argv[]) {
         return -1;
     }
     
-    int width, height, nrChannels;
-        stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
-    unsigned char *data = stbi_load(std::filesystem::path("resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
-    // data是RGBRGBRGB
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    printf("%d, %d\n", width, height);
+    //让其在Apple的retina显示屏上也能正常显示
     
+    //int width, height, nrChannels;
+        stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis.
+    //unsigned char *data = stbi_load(std::filesystem::path("resources/textures/container.jpg").c_str(), &width, &height, &nrChannels, 0);
+    // data是RGBRGBRGB
+    Bitmap bitmap(width, height);
+    unsigned char *ptr = bitmap.get_ptr();
+    kernel(ptr, bitmap.x, bitmap.y);
     
     while(!glfwWindowShouldClose(window))
     {
@@ -77,8 +102,7 @@ int main(int argc, const char * argv[]) {
         // 渲染
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-        
-        // glDrawPixels();
+        glDrawPixels(bitmap.x, bitmap.y, GL_RGBA, GL_UNSIGNED_BYTE, bitmap.pixels);
         
         // 检查并调用事件，交换缓冲
         glfwSwapBuffers(window);
