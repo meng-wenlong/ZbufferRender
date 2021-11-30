eec77a1e832771932921fcfe0e92a9e6a7bcda26
Wenlong Meng <59216317+GYolanda@users.noreply.github.com>
2021-11-30 13:42:27 +0800

    add rotation

--- ZBuffer/main.cpp
+++ ZBuffer/main.cpp
@@ -106,8 +106,9 @@ int main(int argc, const char * argv[]) {
     HierarchyZbuffer hierarchyZbuffer(width);
     
     //verts转化为屏幕像素坐标
-    double xc = mesh.bounding_sphere_c.x;
-    double yc = mesh.bounding_sphere_c.y;
+    double xc = (mesh.bounding_box[0].x + mesh.bounding_box[1].x)/2;
+    double yc = (mesh.bounding_box[0].y + mesh.bounding_box[1].y)/2;
+    //double zc = (mesh.bounding_box[0].z + mesh.bounding_box[1].z)/2;
     vector<vec3f> zoomed_verts;
     for (int i=0; i<mesh.verts.size(); i++) {
         vec3f temp;
@@ -117,8 +118,8 @@ int main(int argc, const char * argv[]) {
         zoomed_verts.push_back(temp);
     }
     vec3f whole_bounding_box[2];
-    whole_bounding_box[0].z = mesh.bounding_box[0].z * ZOOM;
-    whole_bounding_box[1].z = mesh.bounding_box[1].z * ZOOM;
+    whole_bounding_box[0].z = (mesh.bounding_box[0].z) * ZOOM;
+    whole_bounding_box[1].z = (mesh.bounding_box[1].z) * ZOOM;
     whole_bounding_box[0].x = 0;
     whole_bounding_box[1].x = width - 1;
     whole_bounding_box[0].y = 0;
@@ -160,13 +161,6 @@ int main(int argc, const char * argv[]) {
         }
         
     }
-//    for (int i=0; i<mesh.faces.size(); i++) {
-//        //判断三角形是否被挡住
-//        bool passZtest = ztest(mesh.faces[i], zoomed_verts, hierarchyZbuffer);
-//        if (passZtest) {
-//            renderOneTriangle(mesh.faces[i], zoomed_verts, mesh.uvs, &tex, hierarchyZbuffer, framebuffer);
-//        }
-//    }
     
     while(!glfwWindowShouldClose(window))
     {
@@ -174,10 +168,69 @@ int main(int argc, const char * argv[]) {
         processInput(window);   //按ESC退出
         
         // 渲染
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        //glDrawPixels(bitmap.x, bitmap.y, GL_RGBA, GL_UNSIGNED_BYTE, bitmap.pixels);
-        //glDrawPixels(width_t, height_t, GL_RGBA, GL_UNSIGNED_BYTE, texture);
+        for (int y=0; y<width; y++) {   //fb清零
+            for (int x=0; x<height; x++) {
+                int offset = x + y*height;
+                ptr[offset*4 + 0] = 255;
+                ptr[offset*4 + 1] = 255;
+                ptr[offset*4 + 2] = 255;
+                ptr[offset*4 + 3] = 255;
+            }
+        }
+        HierarchyZbuffer hierarchyZbuffer(width); //重新初始化Zbuffer
+        //旋转zoomed_verts
+        static double theta = 0;
+        for (int i=0; i<zoomed_verts.size(); i++) {
+            zoomed_verts[i].x -= width/2;
+            double xt = zoomed_verts[i].x * cos(theta) + zoomed_verts[i].z * sin(theta);
+            double zt = zoomed_verts[i].z * cos(theta) - zoomed_verts[i].x * sin(theta);
+            zoomed_verts[i].x = xt + width/2;
+            zoomed_verts[i].z = zt;
+            if (zt > whole_bounding_box[1].z) {
+                whole_bounding_box[1].z = zt;
+            }
+            if (zt < whole_bounding_box[0].z) {
+                whole_bounding_box[0].z = zt;
+            }
+        }
+        theta += 0.01;
+        
+        //建立场景八叉树
+        OctreeNode ot(whole_bounding_box, 20);
+        
+        for (int i=0; i<mesh.faces.size(); i++) {
+            ot.insrt(mesh.faces.data() + i, zoomed_verts.data());
+        }
+        printf("INFO: Building tree finished!\n");
+        
+        //开始绘制（每个三角形）
+        stack<OctreeNode *> nodeStack;
+        nodeStack.push(&ot);
+        OctreeNode *node;
+        while (!nodeStack.empty()) {
+            node = nodeStack.top();
+            //process
+            bool passZtest = cubeZtest(node, zoomed_verts, hierarchyZbuffer);
+            nodeStack.pop();
+            if (!passZtest) {
+                //pass
+            } else if(node->divided == true) {
+                nodeStack.push(node->nwb);
+                nodeStack.push(node->neb);
+                nodeStack.push(node->swb);
+                nodeStack.push(node->seb);
+                nodeStack.push(node->nwf);
+                nodeStack.push(node->nef);
+                nodeStack.push(node->swf);
+                nodeStack.push(node->sef);
+            } else {
+                for (int i=0; i<node->faces.size(); i++) {
+                    renderOneTriangle(*(node->faces[i]), zoomed_verts, mesh.uvs, &tex, hierarchyZbuffer, framebuffer);
+                }
+            }
+            
+        }
+        
         glDrawPixels(width, height, GL_RGBA, GL_UNSIGNED_BYTE, framebuffer.get_ptr());
         
         // 检查并调用事件，交换缓冲
